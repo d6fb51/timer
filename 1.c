161a92259ba399219eb525135008c8aaee8d7d19
d6fb51 <d6fb51@gmail.com>
2026-07-28 20:19:38 +0400

    just some cosmetic changes

--- 1.c
+++ 1.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
+
 #include <unistd.h>
+
 #include <time.h>
+
 int main(void){
-int z=0;
-int x=0;
-for(int y=0;y!=-1;y++) {
-sleep(1);
-if(y==60){
-z++;
-y=0;
+  int m=0;
+  int h=0;
+  for(int s=0;s!=-1;s++) {
+  sleep(1);
+  if(s==60){
+    m++;
+    s=0;
+            }
+    if(m==60){
+    h++;
+    m=0;
 }
-if(z==60){
-x++;
-z=0;
-}
-
-printf("%d:%d:%d\n",x,z,y);
+printf("%2d:%2d:%2d\n",h,m,s);
 fflush(stdout);
-
-
-
 }
 }
